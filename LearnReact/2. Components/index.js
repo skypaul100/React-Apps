d244d6f98069d19799b5779f2537f0ed1388a98c
skypaul100 <61193125+skypaul100@users.noreply.github.com>
2023-04-11 14:57:55 +0530

    Update index.js

--- LearnReact/2. Components/index.js
+++ LearnReact/2. Components/index.js
@@ -1,1 +1,14 @@
-import
+import React from "react";
+import ReactDOM from "react-dom";
+// import Heading from "./Heading"; //not require to give .jsx
+// import List from "./List";
+// import Para from "./Para";
+import App from "./App.js";// using class i.e App.js or use below
+//import App from "./App.jsx";//uses function to export using App.jsx
+ReactDOM.render(
+  <>
+    <App/>
+  </>,
+
+  document.getElementById("root")
+);
