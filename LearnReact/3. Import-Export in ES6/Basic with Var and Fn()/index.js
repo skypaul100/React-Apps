e2defadb108c70cd6cfb1d741eb4900d87fbce4e
skypaul100 <61193125+skypaul100@users.noreply.github.com>
2023-04-11 16:30:09 +0530

    Create index.js

---
+++ LearnReact/3. Import-Export in ES6/Basic with Var and Fn()/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import name1, {name0 , name2 ,fnName } from "./App";
+//1st name1 is default therefore outside braces and name can be anything als.
+//Inside braces are not default and name must match the var/fn name in exporting file
+
+ReactDOM.render(
+  <div>
+    <ol>Import/Export in ES6
+      <li>{name0()}</li>
+      <li>{name1}</li>
+      <li>{name2}</li>
+      <li>{fnName ()}</li>
+    </ol>
+  </div>,
+  document.getElementById("root")
+);
