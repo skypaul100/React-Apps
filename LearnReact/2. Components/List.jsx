cd15094373c329572727748f1590405098ea71ed
skypaul100 <61193125+skypaul100@users.noreply.github.com>
2023-04-11 15:01:33 +0530

    Create List.jsx

---
+++ LearnReact/2. Components/List.jsx
@@ -0,0 +1,14 @@
+import React from "react";//only react is used to handle the .jsx files
+
+function List()
+{
+    return (
+        <ol>
+        <li>chennai</li>
+        <li>mumbai</li>
+        <li>kolkata</li>
+        <li>delhi</li>
+      </ol>
+    );
+}
+export default List;
