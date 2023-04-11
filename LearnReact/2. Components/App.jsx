89c226b286f2e0a433e2eaa7d7a590be877832c0
skypaul100 <61193125+skypaul100@users.noreply.github.com>
2023-04-11 15:05:50 +0530

    Update App.jsx

--- LearnReact/2. Components/App.jsx
+++ LearnReact/2. Components/App.jsx
@@ -1,3 +1,11 @@
+//use this App.jsx file to import components
+//or use the App.js file.
+//Both works fine in importing components
+//Just slight change in syntax
+//App.js uses Class and render() 
+//App.jsx uses function
+
+
 //Component file name  always starts with capital letter then .jsx
 import React from "react";
 import Heading from "./Heading"; //not require to give .jsx
