4906c1d634eec143d2eab82ab72a30a09c3ecc08
skypaul100 <61193125+skypaul100@users.noreply.github.com>
2023-04-11 15:07:18 +0530

    Update index.js

--- LearnReact/2. Components/index.js
+++ LearnReact/2. Components/index.js
@@ -1,3 +1,5 @@
+#Driver_code
+
 import React from "react";
 import ReactDOM from "react-dom";
 // import Heading from "./Heading"; //not require to give .jsx
@@ -5,6 +7,7 @@ import ReactDOM from "react-dom";
 // import Para from "./Para";
 import App from "./App.js";// using class i.e App.js or use below
 //import App from "./App.jsx";//uses function to export using App.jsx
+//both workks same
 ReactDOM.render(
   <>
     <App/>
