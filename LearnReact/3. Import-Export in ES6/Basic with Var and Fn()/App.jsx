d66c69688fd0e047e62761cf1e4043f46508aa82
skypaul100 <61193125+skypaul100@users.noreply.github.com>
2023-04-11 16:31:28 +0530

    Create App.jsx

---
+++ LearnReact/3. Import-Export in ES6/Basic with Var and Fn()/App.jsx
@@ -0,0 +1,17 @@
+const name0=() => {  // arrow func.
+  let name0="Alpha";
+  return name0;
+}
+
+const name1="Beta";
+
+const name2="Delta";
+
+const fnName= ()=>{
+  return ("Hello");
+}
+
+
+
+export default name1;  // only 1 as default
+export {name0 , name2 , fnName};// rest all at a time but inside braces.
