3d2679486788136cbcb4b3b0de74a238b057f75b
skypaul100 <61193125+skypaul100@users.noreply.github.com>
2023-04-11 15:51:55 +0530

    Create index.js

---
+++ LearnReact/11. Greetings&Time-React/Using components/index.js
@@ -0,0 +1,9 @@
+//Main js file
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//Use same css and html file from prev folder
